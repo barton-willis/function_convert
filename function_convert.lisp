c1671199478ee6d7e0ab28b8d839712afe3c4a2a
Barton Willis <willisb@unk.edu>
2026-01-24 21:18:12 -0600

    converters for abs

--- function_convert.lisp
+++ function_convert.lisp
@@ -253,4 +253,17 @@ the function symbol."
 (define-converter (%erf %hypergeometric) (x)
   "Convert erf(x) into (2*x/sqrt(pi))*hypergeometric([1/2],[3/2],-x^2)."
   (let ((z (car x)) ($hypergeometric_representation t)) (ftake '%erf x)))
-  
+
+  ;;; abs
+
+  (define-converter (mabs %signum) (x)
+  "Convert abs(x) into x*signum(x)."
+  (let ((z (car x)))
+    (mul z (ftake '%signum z))))
+
+  (define-converter (mabs %integral) (x)
+  "Convert abs(x) into integral(1/2, t, -x, x)."
+  (let* ((z (car x))
+         (t ($gensym)))
+    (ftake '%integrate (div 1 2) t (mul -1 z) z)))
+
